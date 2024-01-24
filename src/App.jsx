8802f6a1a2ee9fc6859fe13e797c06b94ed2eda5
Andrew James <mygithubacc3198@gmail.com>
2024-01-24 18:31:35 +0530

    initial commit:project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-red-500 text-center text-xl">
+                Weather App project Setup
+            </h1>
+        </>
+    );
+}
+
+export default App;
