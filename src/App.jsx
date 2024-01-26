7badbb96f70ecb175cd1d47e9f5fbea982fabf5d
Andrew James <mygithubacc3198@gmail.com>
2024-01-26 23:36:19 +0530

    Setting up the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useEffect, useState } from "react";
-import { useWeatherData } from "../hooks/useWeatherData";
-
 function App() {
-    const [input, setInput] = useState("");
-    const [city, setCity] = useState("");
-    const weatherData = useWeatherData(city);
-
-    function handleOnSubmit(e) {
-        e.preventDefault();
-        setCity(input);
-        setInput("");
-    }
-
-    if (weatherData) console.log(weatherData);
-
     return (
         <>
-            <h1 className="text-red-500 text-center text-xl">
-                Weather App project Setup
-                <div>
-                    <h1>Temperatue: {weatherData.temperature}</h1>
-                    <form onSubmit={handleOnSubmit}>
-                        <input
-                            type="text"
-                            value={input}
-                            onChange={(e) => setInput(e.target.value)}
-                            placeholder="enter city..."
-                        />
-                    </form>
+            <section className="flex justify-center items-center border-2 border-green-500 min-h-screen">
+                <div className="text-center max-w-[400px] bg-[#200fb7] rounded-3xl">
+                    <div className="p-8">
+                        <h1 className="text-slate-300 text-[1.85rem]">Delhi</h1>
+                    </div>
+                    <div className="h-[200px] w-[200px] flex justify-center items-center border-2 rounded-full mx-auto">
+                        <h1 className="text-[4rem] text-slate-200">14°C</h1>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="text-[1.5rem] font-bold text-slate-200">
+                            Lorem, ipsum.
+                        </h1>
+                        <p className="text-lg font-semibold text-slate-300">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Incidunt, vitae. Fuga consequuntur recusandae
+                            architecto molestias est minus laudantium obcaecati
+                            aspernatur.
+                        </p>
+                    </div>
                 </div>
-            </h1>
+            </section>
         </>
     );
 }
